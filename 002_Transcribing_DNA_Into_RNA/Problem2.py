0499474e05dc52c04c283a3a43c9901af22dc5b2
Timothy Mahajan <tmahajan@iu.edu>
2017-10-03 00:56:57 -0400

    Add files via upload

---
+++ 002_Transcribing_DNA_Into_RNA/Problem2.py
@@ -0,0 +1,6 @@
+#Problem 2: Transcribing DNA into RNA
+#Given: A DNA string t having length at most 1000 nt.
+#Return: The transcribed RNA string of tt.
+
+str = "GATGGAACTTGACTACGTAAATT"
+print (str.replace("T", "U"))
