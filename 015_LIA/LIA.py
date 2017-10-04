3fc121947aa7c8a247efc3a7bfd0489573c1bcdd
Timothy Mahajan <tmahajan@iu.edu>
2017-10-04 15:09:17 -0400

    Update LIA.py

--- 015_LIA/LIA.py
+++ 015_LIA/LIA.py
@@ -1,4 +1,7 @@
-#Independent Alleles
+#Problem 15: Independent Alleles
+#Given: Two positive integers k (k≤7k≤7) and N (N≤2kN≤2k). In this problem, we begin with Tom, who in the 0th generation has genotype Aa Bb. Tom has two children in the 1st generation, each of whom has two children, and so on. Each organism always mates with an organism having genotype Aa Bb.
+#Return: The probability that at least N Aa Bb organisms will belong to the k-th generation of Tom's family tree (don't count the Aa Bb mates at each level). Assume that Mendel's second law holds for the factors.
+
 import math
 
 def prob(k,n):
