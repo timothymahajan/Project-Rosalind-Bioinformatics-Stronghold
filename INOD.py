31b578eb2452329fccae1bd49f55b8cf090345a7
Timothy Mahajan <tmahajan@iu.edu>
2017-10-08 19:42:07 -0400

    Add files via upload

---
+++ INOD.py
@@ -0,0 +1,7 @@
+#Problem 35: Counting Phylogenetic Ancestors
+#Given: A positive integer n(3≤n≤10000).
+#Return: The number of internal nodes of any unrooted binary tree having n leaves.
+
+n = 4
+#There will always be n-2 nodes in an unrooted binary tree.
+print(n-2)
