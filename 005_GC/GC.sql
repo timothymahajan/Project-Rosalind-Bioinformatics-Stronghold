65692a291e430ec92cfe563973558ddbfea56a8d
Timothy Mahajan <tmahajan@iu.edu>
2017-10-08 00:59:51 -0400

    Update and rename Problem5.sql to GC.sql

--- 005_GC/GC.sql
+++ 005_GC/GC.sql
@@ -1,3 +1,7 @@
+--Problem 5: Computing GC Content
+--Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+--Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
+
 declare @input varchar(max) = 
 '
 >Rosalind_6404
