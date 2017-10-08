faf2393ae47d2ec69103dd06feff7e8de3e40fac
Timothy Mahajan <tmahajan@iu.edu>
2017-10-07 23:37:26 -0400

    Update REVC.R

--- 003_REVC/REVC.R
+++ 003_REVC/REVC.R
@@ -1,3 +1,7 @@
+#Problem 3: Complementing a Strand of DNA
+#Given: A DNA string s of length at most 1000 bp.
+#Return: The reverse complement sc of s.
+
 require(Biostrings)
 dna = DNAString("AAAACCCGGT")
 comp = as.character(reverseComplement(dna))
