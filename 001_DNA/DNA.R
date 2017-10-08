dec410259463590cda997a361a07c184174ea6bd
Timothy Mahajan <tmahajan@iu.edu>
2017-10-07 20:42:21 -0400

    Update DNA.R

--- 001_DNA/DNA.R
+++ 001_DNA/DNA.R
@@ -1,3 +1,7 @@
+#Problem 1: Counting DNA Nucleotides
+#Given: A DNA string ss of length at most 1000 nt.
+#Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in ss.
+
 library(stringr)
 seq = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
 str_count(seq, c("A","C","G","T"))
