4b02a7db16b5067e8d40557eef7649465db02cfc
Timothy Mahajan <tmahajan@iu.edu>
2017-10-14 21:02:31 -0400

    Update FIB.R

--- 004_FIB/FIB.R
+++ 004_FIB/FIB.R
@@ -1,4 +1,6 @@
-
+#Problem 4: Rabbits and Recurrence Relations
+#Given: Positive integers n≤40n≤40 and k≤5k≤5.
+#Return: The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).
 
 fibonacci <- function(a1 = 1, a2 = 1, t, k = 1) {
   vec<-vector()
