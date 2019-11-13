762d04d94fa6aadcfc64f251974aef6898bf3d00
Timothy Mahajan <tim.mahajan@gmail.com>
2019-11-13 20:46:07 +0000

    Update DNA.py

--- 001_DNA/DNA.py
+++ 001_DNA/DNA.py
@@ -2,20 +2,8 @@
 #Given: A DNA string ss of length at most 1000 nt.
 #Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in ss.
 
-def countSymbol (str):
-    aCount = 0
-    cCount = 0
-    gCount = 0
-    tCount = 0
-    for i in str:
-        if (i == 'A'):
-            aCount = aCount + 1
-        elif (i == 'C'):
-            cCount = cCount + 1
-        elif (i == 'G'):
-            gCount = gCount + 1
-        else:
-            tCount = tCount + 1
-    return [aCount, cCount, gCount, tCount]
 
-print(countSymbol("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"))
+def countSymbol (string):
+    return str(string.count("A")) + " " + str(string.count("G")) + " " + str(string.count("C")) + " " + str(string.count("T"))
+
+print(countSymbol("TAGGAGTAGTCCTAGGAGGGACTTCAAGAGGAGTGGTCGGGCGAGCACTCACCGTCGTCTGAAGTTAACCTGA"))
